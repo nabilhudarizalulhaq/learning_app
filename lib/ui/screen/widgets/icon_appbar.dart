41e3a3eefe7b82b1cd8ce5067f964c2c32e5bf73
Nabil Huda Rizalul Haq <nabilhudarizalulhaq@192.168.1.45>
2023-11-13 15:55:43 -0800

    icon notification hom

--- lib/ui/screen/widgets/icon_appbar.dart
+++ lib/ui/screen/widgets/icon_appbar.dart
@@ -5,25 +5,25 @@ class IconAppbar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(margin: const EdgeInsets.only(left: 8),
+    return Container(
+      margin: const EdgeInsets.only(left: 8),
       width: 20,
       height: 20,
       decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        color: const Color.fromARGB(255, 255, 255, 255),
-        border: Border.all(
-          width: 2,
-          color: const Color(0xffBEBAB3),
-        )
+          shape: BoxShape.circle,
+          color: const Color.fromARGB(255, 255, 255, 255),
+          border: Border.all(
+            width: 2,
+            color: const Color(0xffBEBAB3),
+          )),
+      child: IconButton(
+        icon: const Icon(Icons.arrow_back_ios_new_rounded),
+        color: Colors.black,
+        iconSize: 22,
+        onPressed: () {
+          Navigator.pop(context);
+        },
       ),
-        child: IconButton(
-          icon: const Icon(Icons.arrow_back_ios_new_rounded),
-          color: Colors.black,
-          iconSize: 22,
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
     );
   }
 }
@@ -34,25 +34,23 @@ class IconNotificationHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: (){},
+      onTap: () {},
       child: Container(
-        margin: const EdgeInsets.only(left: 8),
-        width: 48,
-        height: 48,
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          color: const Color.fromARGB(255, 255, 255, 255),
-          border: Border.all(
-            width: 2,
-            color: const Color(0xffBEBAB3),
-          )
-        ),
+          margin: const EdgeInsets.only(left: 70),
+          width: 48,
+          height: 48,
+          decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: const Color.fromARGB(255, 255, 255, 255),
+              border: Border.all(
+                width: 2,
+                color: const Color(0xffBEBAB3),
+              )),
           child: const Image(
             image: AssetImage(
               'assets/notification_home.png',
             ),
-          )
-      ),
+          )),
     );
   }
 }
